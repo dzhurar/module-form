befc11d3ee82cefac510c7805e0a20024652d676
dzhurar <142822867+dzhurar@users.noreply.github.com>
2025-02-02 20:06:58 +0200

    seems done

---
+++ src/js-modules/form-send.js
@@ -0,0 +1,22 @@
+// export function saveData(event){
+//     event.preventDefault();
+
+//     alert('kill');
+// };
+export const saveData = (event) => {
+    event.preventDefault();
+
+    dataBase.name = name.value;
+    dataBase.email = email.value;
+    dataBase.comment = comment.value;
+    form.reset();
+    console.log(dataBase.name, dataBase.email, dataBase.comment);
+}
+const name = document.getElementById('nameInp');
+const email = document.getElementById('emailInp');
+const comment = document.getElementById('commentInp');
+const dataBase = {
+    name: '',
+    email: '',
+    comment: '',
+}
